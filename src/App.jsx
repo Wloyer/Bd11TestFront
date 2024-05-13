b5ea60886c6de47d63e4b4a569113d3d8ac8c26e
Jokast38 <jokast38@gmail.com>
2024-05-13 17:38:11 +0200

    chore: Update react dependencies and add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Page404 from './pages/404';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
 
     </>
---
+++ src/pages/404.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Page404() {
+    return (
+        <>
+            <h1>404</h1>
+            <div className="cloak__wrapper">
+                <div className="cloak__container">
+                    <div className="cloak">
+                        <div className="info">
+                            <h2>We can't find that page</h2>
+                            <p>
+                                We're fairly sure that page used to be here, but seems to have gone missing. We do apologise on its behalf.
+                            </p>
+                            <a href="/" target="_blank" rel="noreferrer noopener">Home</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Page404
