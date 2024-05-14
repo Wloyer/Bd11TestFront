6c7df0cca912a3ab7a9b10b6df1191da7c276477
Jokast38 <jokast38@gmail.com>
2024-05-14 16:21:21 +0200

    chore: Add Inscription page and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Page404 from './pages/404';
 import Connexion from './pages/Connexion';   
-
+import Inscription from './pages/Inscription';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Page404 />} />
         <Route path="/connexion" element={<Connexion />} />
+        <Route path="/inscription" element={<Inscription />} />
       </Routes>
 
     </>
