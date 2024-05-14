43d3f2a0b4286ac995e2ff29ac130dc81d69a297
Jokast38 <jokast38@gmail.com>
2024-05-14 14:35:21 +0200

    chore: Add Connexion and Inscription pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Page404 from './pages/404';
+import Connexion from './pages/Connexion';   
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Page404 />} />
+        <Route path="/connexion" element={<Connexion />} />
       </Routes>
 
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function App() {
       <Title />
       <Button href="https://dribbble.com/shots/23181733-Blurry-Glassmorphic-Modern-Landing-page" target="_blank" className="first">View Source</Button>
       <Button href="https://youtu.be/1pW_sk-2y40" target="_blank" className="sec">Découvrir Nous Event</Button>
-      <Button href="https://x.com/juxtopposed" target="_blank" className="third">Se Connecter</Button>
+      <Button href="/connexion" target="_blank" className="third">Se Connecter</Button>
     </div>
   );
 }
