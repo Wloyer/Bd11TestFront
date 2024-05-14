b2f85a7782d2a13ef1997642c42410c4bc11bb91
Jokast38 <jokast38@gmail.com>
2024-05-14 11:47:02 +0200

    style: Improve center alignment and button styling on 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,16 +4,16 @@ import '../styles/404.css';
 function Page404() {
     return (
         <>
-            <h1>404</h1>
-            <div className="cloak__wrapper">
+            <h1 style={{ textAlign: 'center' }}>404</h1>
+            <div className="cloak__wrapper" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <div className="cloak__container">
                     <div className="cloak">
-                        <div className="info">
+                        <div className="info" style={{ textAlign: 'center' }}>
                             <h2>We can't find that page</h2>
                             <p>
                                 We're fairly sure that page used to be here, but seems to have gone missing. We do apologise on its behalf.
                             </p>
-                            <a href="/" target="_blank" rel="noreferrer noopener">Home</a>
+                            <button style={{ margin: '0 auto', textAlign: 'center' }}> <a style={{ }} href="/">Home</a></button>
                         </div>
                     </div>
                 </div>
